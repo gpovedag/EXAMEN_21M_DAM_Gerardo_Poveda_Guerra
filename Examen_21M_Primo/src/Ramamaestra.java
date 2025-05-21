f3d170c7c622de9b96a1e65db25433b60f7878e0
ALUMNOS <ALUMNOS@P608>
2025-05-21 11:53:09 +0200

    a

---
+++ Examen_21M_Primo/src/Ramamaestra.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ramamaestra {
+	 public static void main(String[] args) {
+		 Scanner entrada = new Scanner (System.in);
+		 int numero = 0;
+		 System.out.println("Buenos días Ceinmark, soy Gerardo");
+		 System.out.println("Introduce un número");
+		 numero = entrada.nextInt();
+	 }
+
+}
