24f21212490bd33c56f23133fb69dbb3c569eda7
ALUMNOS <ALUMNOS@P608>
2025-05-21 12:05:53 +0200

    abcd

--- Examen_21M_Primo/src/Ramamaestra.java
+++ Examen_21M_Primo/src/Ramamaestra.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ramamaestra {
 	 public static void main(String[] args) {
-		System.out.println("6 no es número primo. Los divisores del número son:1,2,3,6");
+		System.out.println("8 no es número primo. Los divisores del número son:1,2,3,4,8");
 	 }
 
 }
